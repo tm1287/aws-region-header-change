e2dc05d739bcf677de867dc2d1c7cb78f30c14b4
tm1287 <tejas.maraliga@gmail.com>
2020-09-27 01:56:18 -0400

    Get region from DOM

--- content.js
+++ content.js
@@ -1,3 +1,13 @@
 const navContainer = document.getElementById("awsc-navigation-container");
+const headerShelf = navContainer.childNodes[0];
+headerShelf.style.backgroundColor = "red";
 
-console.log((navContainer.childNodes[0].style.backgroundColor = "red"));
+const regionMenuParent = document.getElementById(
+  "awsc-navigation__more-menu--list"
+);
+
+let menuButton = regionMenuParent.querySelector(
+  '[data-testid = "awsc-nav-regions-menu-button"]'
+);
+
+let region = menuButton.innerHTML.match(/(?<=>)(.*?)(?=<)/g)[0];
