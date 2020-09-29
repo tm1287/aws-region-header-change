13b2e2dfe6b73f70097b0cc44442aaab1b465656
tm1287 <tejas.maraliga@gmail.com>
2020-09-29 00:59:47 -0400

    Document content script

--- background.js
+++ background.js
@@ -1,9 +1,3 @@
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ status: "Active" }, function () {
-    console.log("The status is active");
-  });
-});
-
 chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
   chrome.declarativeContent.onPageChanged.addRules([
     {
--- content.js
+++ content.js
@@ -20,9 +20,15 @@ const regions = {
   "São Paulo": { fontColor: "#294292", backgroundColor: "#212125" },
 };
 
+/**
+ * Gets the nav container from the dom and isolates the header shelf containing the css styles
+ */
 const navContainer = document.getElementById("awsc-navigation-container");
 const headerShelf = navContainer.childNodes[0];
 
+/**
+ * Start to isolate the region dropdown to fetch the region data
+ */
 const regionMenuParent = document.getElementById(
   "awsc-navigation__more-menu--list"
 );
@@ -31,7 +37,13 @@ let menuButton = regionMenuParent.querySelector(
   '[data-testid = "awsc-nav-regions-menu-button"]'
 );
 
+/**
+ * Parse html using regex for region because element ids/classes are obfuscated
+ */
 let currentRegion = menuButton.innerHTML.match(/(?<=>)(.*?)(?=<)/g)[0];
 console.log(currentRegion);
 
+/**
+ * Set the background color of the header based on the predefined styles
+ */
 headerShelf.style.backgroundColor = regions[currentRegion].fontColor;
