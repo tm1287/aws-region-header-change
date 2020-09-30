3b974fddd3bc9f4a5b96804f933e3c8ee3394dc9
tm1287 <tejas.maraliga@gmail.com>
2020-09-30 02:12:15 -0400

    Remove unused popup html/js

--- popup.js
+++
@@ -1,9 +0,0 @@
-let region = document.getElementById("region");
-
-chrome.storage.local.get("region", function (data) {
-  region.textContent = data.region;
-});
-
-chrome.storage.local.get("currentColor", function (data) {
-  region.style.backgroundColor = data.currentColor;
-});
